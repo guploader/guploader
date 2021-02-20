8e974d139e843928b0b35a8e67a2ea627b56a1e4
geeeger <lph1988520@sina.com>
2021-02-20 16:53:12 +0800

    chore: init project

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  rootDir: 'packages',
+  coverageDirectory: '../coverage',
+  coverageReporters: ['cobertura', 'text'],
+  collectCoverageFrom: ['**/src/**/*.ts'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      { suiteName: 'jest tests', suiteNameTemplate: '{filepath}' },
+    ],
+  ],
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts'],
+  globals: {
+    skipBabel: true,
+  },
+};
